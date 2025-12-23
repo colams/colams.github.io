69cb4fe77ada0cddea9db6ecb7ffe57a12e483c6
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-12-23 14:28:24 +0800

    修改代碼路徑

---
+++ js/chunk-B3xSzs2G.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./chunk-BengT3oa.js";import{y as o,v as s,o as t}from"./chunk-Bmj9xSPk.js";const n=r({},[["render",function(r,n){const e=s("router-view");return t(),o(e)}]]);export{n as default};
